3bf037273392c98253454b6cefe495fdac876898
Jose Douglas Ramirez E <chibchombiano26@hotmail.com>
2015-05-07 21:39:29 -0500

    click listados

---
+++ www/js/directives/list/listClick.js
@@ -0,0 +1,24 @@
+angular.module('starter').
+directive('listClick', ['$parse',function ($parse) {
+	return {
+		restrict: 'A',
+		link: function (scope, el, attrs) {	
+			var mostrar = true;			
+			el.on('click', function(){
+				var pading = $(el).attr( "list-click" )
+
+				if(mostrar){
+					mostrar = false;
+					$(el).find('.item-content').css('-webkit-transform','translate3d(' + pading +', 0px, 0px)');
+					$(el).find('.item-options').removeClass('invisible');
+				}
+				else{
+					mostrar = true;
+					$(el).find('.item-content').removeAttr( 'style' );
+					$(el).find('.item-options').addClass('invisible');	
+				}
+				console.log("click");
+			})			
+		}
+	};
+}])
