c531b51b99a26fe1903209d562bc26d6d3b57730
Jose Douglas Ramirez E <chibchombiano26@hotmail.com>
2015-05-19 15:31:49 -0500

    Autocompletar inicio

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'winjs', 'ngjsColorPicker', 'ngCordova'])
+angular.module('starter', ['ionic', 'winjs', 'ngjsColorPicker', 'ngCordova', 'ion-autocomplete', 'ion-google-place'])
 
 .run(function($ionicPlatform, $rootScope) {
   $ionicPlatform.ready(function() {    
--- www/js/controllers/Cie_Cups/Cie_Cups.js
+++ www/js/controllers/Cie_Cups/Cie_Cups.js
@@ -3,7 +3,8 @@ angular.module('starter')
 	function ($scope, sharedSignatureHttpHelper, $q) {
 
 		var listado = [];
-		$scope.listado = [];
+		$scope.listado = [];		
+		$scope.textoBuscar;
 
 		function load(){
 		    $q.all([sharedSignatureHttpHelper.getAll("TRCIE", '?tn=TRCIE&sv=2014-02-14&si=Shared&sig=uHj3pBPqCIIVhYsNXaiSdValKIqu9Q0dydf9%2FPzePdk%3D'), 
@@ -34,8 +35,24 @@ angular.module('starter')
 
 			};
 		}
-	
 
+		$scope.getTags = function (query) {
+			query = query.toUpperCase();
+			var array = _.filter(listado, function(item){
+						  //return (item.Descripcion.indexOf(query) > -1 || item.Codigo.indexOf(query));
+						  return item.Descripcion.indexOf(query) > -1;
+						});
+            return array;
+
+       }
+
+       $scope.callbackMethod = function (callback) {
+		    // print out the selected item
+		    console.log(callback.item); 
+
+		    // print out the selected items if the multiple select flag is set to true and multiple elements are selected
+		    //console.log(callback.selectedItems); 
+	   }
 
     load();
 	
